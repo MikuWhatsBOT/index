76579c1da664cf8c7986e16af8cb46dddcefe632
MikuWhatsBOT <80510902+MikuWhatsBOT@users.noreply.github.com>
2021-04-22 04:56:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/index.html')
 })
 
 router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
+    res.sendFile(__path + '/index.html')
 })
 
 router.get('/config', (req, res) => {
